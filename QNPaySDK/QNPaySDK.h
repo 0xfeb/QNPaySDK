244504f23a2b3d15daad6b6409502720928ff4af
王渊鸥 <wangyuanou@foxmail.com>
2017-04-21 16:57:20 +0800

    Initial Commit

---
+++ QNPaySDK/QNPaySDK.h
@@ -0,0 +1,19 @@
+//
+//  QNPaySDK.h
+//  QNPaySDK
+//
+//  Created by 王渊鸥 on 2017/4/21.
+//  Copyright © 2017年 王渊鸥. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for QNPaySDK.
+FOUNDATION_EXPORT double QNPaySDKVersionNumber;
+
+//! Project version string for QNPaySDK.
+FOUNDATION_EXPORT const unsigned char QNPaySDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <QNPaySDK/PublicHeader.h>
+
+
